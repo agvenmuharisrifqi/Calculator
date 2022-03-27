b5e9095ae8de1c1e969edf6f1fd3d81a8475e2d9
agvenmuharisrifqi <agven25@gmail.com>
2022-03-27 21:32:09 +0700

    Update Phone Layout

--- script.js
+++ script.js
@@ -37,14 +37,14 @@ for (let item = 0; item < navItem.length; item++) {
 /**
  * @Function Interface Calculator
  */
-// const re = /[^x\/\-\+\d]/gmi;
+const re = /[^x\/\-\+\d]/gmi;
 const inputNum = document.getElementById("number");
-// inputNum.addEventListener('keyup', (e)=>{
-//     let val = inputNum.value;
-//     let res = val.replace(re, "");
-//     inputNum.value = res;
-// })
+inputNum.addEventListener('keyup', (e)=>{
+    let val = inputNum.value;
+    let res = val.replace(re, "");
+    inputNum.value = res;
+})
 
-let h = window.innerHeight;
-let w = window.innerWidth;
-inputNum.value = `L ${w}, T ${h}`;
+// let h = window.innerHeight;
+// let w = window.innerWidth;
+// inputNum.value = `L ${w}, T ${h}`;
