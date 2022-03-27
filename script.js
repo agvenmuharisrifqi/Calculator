921e6648619d2bd946263edf23fab8bb90b7fa5c
agvenmuharisrifqi <agven25@gmail.com>
2022-03-27 16:09:09 +0700

    Create Standard Calculator Interface

--- script.js
+++ script.js
@@ -7,10 +7,26 @@ const navToggle = document.getElementById("navbar-toggle");
 navToggle.addEventListener('click', ()=>{
     navBar.classList.toggle("active");
 })
-window.addEventListener("resize", ()=>{
+
+const windowWidth = ()=>{
     if (window.innerWidth < 992){
         navBar.classList.add("active");
     }else {
-        navBar.classList.remove("active");
+        navBar.classList.remove("active");  
     }
+}
+
+
+
+/**
+ * @Function Calculator
+ */
+const re = /\s|\D/gmi;
+const inputNum = document.getElementById("number");
+inputNum.addEventListener('keyup', (e)=>{
+    var val = inputNum.value;
+    let res = val.replace(re, "");
+    inputNum.value = res;
 })
+
+console.log(document.getElementById('coba').textContent);
